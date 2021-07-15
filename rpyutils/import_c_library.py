bdbe4d64138458f357c0a61540600973c05fa7ee
Christophe Bedard <bedard.christophe@gmail.com>
2021-07-15 19:27:13 -0400

    Update troubleshooting links to docs.ros.org
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- rpyutils/import_c_library.py
+++ rpyutils/import_c_library.py
@@ -43,17 +43,14 @@ def import_c_library(name: str, package: Optional[str] = None):
             expected_path = Path(__file__).parents[1] / (
                 name[1:] + sysconfig.get_config_var('EXT_SUFFIX'))
             assert not expected_path.is_file()
+            link = f'https://docs.ros.org/en/rolling/Guides/Installation-Troubleshooting.html#import-failing-without-library-present-on-the-system'  # noqa: E501
             e.msg += \
                 f"\nThe C extension '{expected_path}' isn't present on the " \
-                "system. Please refer to 'https://index.ros.org/doc/ros2/" \
-                'Troubleshooting/Installation-Troubleshooting/#import-' \
-                "failing-without-library-present-on-the-system' for " \
-                'possible solutions'
+                f"system. Please refer to '{link}' for possible solutions"
         if e.path is not None and os.path.isfile(e.path):
+            link = 'https://docs.ros.org/en/rolling/Guides/Installation-Troubleshooting.html#import-failing-even-with-library-present-on-the-system'  # noqa: E501
             e.msg += \
                 f"\nThe C extension '{e.path}' failed to be imported while " \
-                "being present on the system. Please refer to 'https://" \
-                'index.ros.org/doc/ros2/Troubleshooting/Installation-' \
-                'Troubleshooting/#import-failing-even-with-library-present-' \
-                "on-the-system' for possible solutions"
+                f"being present on the system. Please refer to '{link}' for " \
+                'possible solutions'
         raise
